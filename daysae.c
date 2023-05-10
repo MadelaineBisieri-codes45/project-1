9cd67e11fb35ff1f0063d31434a7cc0d2f128c80
Madelaine Bisieri <123293686+MadelaineBisieri-codes45@users.noreply.github.com>
2023-05-10 20:14:47 +0300

    Add files via upload

---
+++ daysae.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a;
+int b;
+int sum;
+printf("first number",&a);
+scanf("%d",&a);
+printf("second number",&b);
+scanf("%d",&b);
+printf("sum",&sum);
+return 0;
+
+
+}
